57c9947f2fda111aaca555c6adf57de36cbc74de
hjmhjm321 <164042388+hjmhjm321@users.noreply.github.com>
2024-05-07 23:19:16 +0800

    期中考试

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,96 +1,63 @@
-package com.example.myapplication
+package com.example.middleexam
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.myapplication.ui.theme.MyApplicationTheme
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MyApplicationTheme {
-                        GreetingImage("Happy Birthday Sams", "From hjm")
-                    }
-                }
+            MaterialTheme{
+
             }
         }
     }
 }
 
 @Composable
-fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
-    Column(
-        verticalArrangement = Arrangement.Center,
-        modifier = modifier
-    ) {
-        Text(
-            text = message,
-            fontSize = 100.sp,
-            lineHeight = 116.sp,
-            textAlign = TextAlign.Center
-        )
-        Text(
-            text = from,
-            fontSize = 36.sp,
-            modifier = Modifier
-                .padding(16.dp)
-                .align(alignment = Alignment.CenterHorizontally)
-        )
-    }
-}
-@Composable
-fun GreetingImage(message: String, from: String){
-    val image = painterResource(id = R.drawable.hjm)
-    Box(Modifier) {
-        Image(
-            painter = image,
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            alpha = 0.5F
-        )
-        GreetingText(
-            message = message,
-            from = from,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(8.dp)
-        )
+fun Navigator(){
+    val navController = rememberNavController()
+    NavHost(navController = navController,startDestination = "Home"){
+        composable("Home"){HomePage(navController)}
+        composable("CommodityPage"){CommodityPage(navController, UserData.commodityList)}
+        composable("EquipmentPage"){EquipmentPage(navController, UserData.equipmentList)}
+        composable("EndPage"){EndPage(navController)}
     }
 }
 
-@Preview(
-    showBackground = true,
-    showSystemUi = true
-)
-
 @Composable
-fun GreetingPreview(){
-    MyApplicationTheme {
-        GreetingImage(message = "Happy Birthday Sam!", from = "From hjm")
+fun HomePage(navController: NavController){
+    Column{
+        Button(onClick = {
+            navController.navigate("CommodityPage")
+        }){
+            Text(text = "CommodityPage")
+        }
+        Button(onClick = {
+            navController.navigate("EquipmentPage")
+        }){
+            Text(text = "EquipmentPage")
+        }
+        Button(onClick = {
+            navController.navigate("EndPage")
+        }){
+            Text(text = "EndPage")
+        }
     }
+}
+@Composable
+@Preview(showSystemUi = true)
+fun previeHost(){
+    Navigator()
 }
